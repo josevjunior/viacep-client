b7edab73cce719d8c72cfa09ebc209aba1d7f63e
josev.junior <josev.junior@outlook.com>
2019-04-03 22:59:24 -0300

    Correção do nome da aplicação

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/viacep-client'));
+app.use(express.static(__dirname + '/dist/viacep-api-client'));
 
 app.use('/*', function(req, res){
-    res.sendFile(path.join(__dirname+'/dist/viacep-client/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/viacep-api-client/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
